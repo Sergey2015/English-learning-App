96401272a9d0e2f8113e07c698b31a20619cf768
Sergey <kusin99@me.com>
2023-06-21 17:44:45 +0300

    1

---
+++ first_app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('Uber pickups in NYC')
+
+
+
+data_load_state = st.text('Little Red Cap. Jacob and Wilhelm Grimm dsfsf, fdgdfgfg')
